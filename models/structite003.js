556a2c3f88e2ee10198eb627bfcc54e57f848806
Eduardo <erlludovico@gmail.com>
2017-07-13 17:06:58 -0300

    Mudando string para nuven

--- models/structite003.js
+++ models/structite003.js
@@ -1,6 +1,6 @@
 var mongoose    =   require("mongoose");
-//mongoose.connect('mongodb://sys:sys@ds157342.mlab.com:57342/erl');
-mongoose.connect('mongodb://localhost:27017/demoDb');
+mongoose.connect('mongodb://sys:sys@ds157342.mlab.com:57342/erl');
+//mongoose.connect('mongodb://localhost:27017/demoDb');
 mongoose.set('debug', true);
 
 // create instance of Schema
